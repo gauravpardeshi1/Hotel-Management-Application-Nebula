8eaee9765beef70cecba9c5f2f03d7e2331af682
Ga1156 <gauravpardeshi068@gmail.com>
2023-07-19 01:38:39 +0530

    first_commit

---
+++ backend/main.py
@@ -0,0 +1,149 @@
+# from flask import Flask, request, jsonify
+# from flask_pymongo import MongoClient
+
+# app = Flask(__name__)
+# # app.config['MONGO_URI'] = 'mongodb+srv://gaurav:pardeshi@cluster0.naivyl2.mongodb.net/?retryWrites=true&w=majority'
+# # mongo = MongoClient(app)
+# client = MongoClient("mongodb+srv://faizanmd:faizan123@cluster0.defifv6.mongodb.net/mohsindb?retryWrites=true&w=majority")
+# db = client["mohsindb"]
+# Host and Property Management
+
+from flask import Flask
+from pymongo import MongoClient
+from flask import Flask, request
+from flask import jsonify
+
+from bson.objectid import ObjectId
+
+        
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://gaurav:pardeshi@cluster0.naivyl2.mongodb.net/ga201?retryWrites=true&w=majority")
+db = client["ga201"]
+class User: # also this will be diffrent according to the project
+    def __init__(self, name, email):
+        self._id = ObjectId()
+        self.name = name
+        self.email = email
+
+def add_user(user_data):
+    users_collection = db["users"]  #   new collection will be there according to the project
+    user = User(user_data["name"], user_data["email"])
+    user_data = {           # data will be diffrent
+        "_id": user._id, 
+        "name": user.name,
+        "email": user.email
+    }
+    users_collection.insert_one(user_data)
+    
+@app.route("/")
+def home():
+ return "Welcome to Python Projects"
+
+@app.route('/hosts', methods=['GET'])
+def get_hosts():
+    hosts = db.hosts.find()
+    host_list = []
+    for host in hosts:
+        host_list.append({
+            'id': str(host['_id']),
+            'name': host['name'],
+            'email': host['email']
+        })
+    return jsonify(host_list)
+
+@app.route('/hosts', methods=['POST'])
+def create_host():
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+
+    host = {
+        'name': name,
+        'email': email
+    }
+
+    inserted_host = db.hosts.insert_one(host)
+
+    return jsonify({'message': 'Host created successfully', 'id': str(inserted_host.inserted_id)})
+
+
+# # Guest Management
+# @app.route('/guests', methods=['GET'])
+# def get_guests():
+#     guests = mongo.db.guests.find()
+#     guest_list = []
+#     for guest in guests:
+#         guest_list.append({
+#             'id': str(guest['_id']),
+#             'name': guest['name'],
+#             'email': guest['email']
+#         })
+#     return jsonify(guest_list)
+
+# @app.route('/guests', methods=['POST'])
+# def create_guest():
+#     data = request.get_json()
+#     name = data.get('name')
+#     email = data.get('email')
+
+#     guest = {
+#         'name': name,
+#         'email': email
+#     }
+
+#     inserted_guest = mongo.db.guests.insert_one(guest)
+
+#     return jsonify({'message': 'Guest created successfully', 'id': str(inserted_guest.inserted_id)})
+
+
+# # Booking Management
+# @app.route('/bookings', methods=['POST'])
+# def create_booking():
+#     data = request.get_json()
+#     guest_id = data.get('guest_id')
+#     property_id = data.get('property_id')
+#     check_in = data.get('check_in')
+#     check_out = data.get('check_out')
+
+#     booking = {
+#         'guest_id': guest_id,
+#         'property_id': property_id,
+#         'check_in': check_in,
+#         'check_out': check_out
+#     }
+
+#     inserted_booking = mongo.db.bookings.insert_one(booking)
+
+#     return jsonify({'message': 'Booking created successfully', 'id': str(inserted_booking.inserted_id)})
+
+
+# # Property Listing
+# @app.route('/properties', methods=['GET'])
+# def get_properties():
+#     location = request.args.get('location')
+#     property_type = request.args.get('type')
+#     host_id = request.args.get('host_id')
+
+#     properties = mongo.db.properties.find()
+
+#     if location:
+#         properties = properties.filter({'location': location})
+#     if property_type:
+#         properties = properties.filter({'type': property_type})
+#     if host_id:
+#         properties = properties.filter({'host_id': host_id})
+
+#     property_list = []
+#     for property in properties:
+#         property_list.append({
+#             'id': str(property['_id']),
+#             'name': property['name'],
+#             'location': property['location'],
+#             'type': property['type'],
+#             'host_id': property['host_id']
+#         })
+#     return jsonify(property_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend/vacation-rental-platform/src/app/app.component.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'vacation-rental-platform'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('vacation-rental-platform');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('vacation-rental-platform app is running!');
+  });
+});
